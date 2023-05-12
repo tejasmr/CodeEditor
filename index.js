24c2cda3cf705475a5fa67e5263dc71151e1fe86
tejasmr <tejasmr@outlook.com>
2023-05-12 21:37:16 +0530

    Added event listener for text area keyup

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 document.title = "Untitled";
+
+const textarea = document.querySelector('textarea')
+const lineNumbers = document.querySelector('.line-numbers')
+
+textarea.addEventListener('keyup', event => {
+    const numberOfLines = event.target.value.split('\n').length
+  
+    lineNumbers.innerHTML = Array(numberOfLines)
+        .fill('<span></span>')
+        .join('')
+})
