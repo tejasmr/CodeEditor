7b1ccf382fa1bb3b295cfb57e73e81c7106deaa0
tejasmr <tejasmr@outlook.com>
2023-05-12 20:27:02 +0530

    Using vanilla JS instead

--- README.md
+++
@@ -1,3 +0,0 @@
-# Code Editor
-
-Just another Code Editor 
--- src/routes/+page.svelte
+++
@@ -1,4 +0,0 @@
-<div class="editor">
-  <div class="line-numbers"><span /></div>
-  <textarea class="codearea" cols="30" rows="10" />
-</div>
